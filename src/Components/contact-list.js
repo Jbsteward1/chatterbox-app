3ad53c36e938bd8e7cd661cd2966289a7b54be4d
Mar'Kez <MarkezBJC@gmail.com>
2021-11-04 10:32:34 -0400

    login update

--- src/Components/contact-list.js
+++ src/Components/contact-list.js
@@ -1,54 +1,54 @@
-import React from 'react';
+// import React from 'react';
 
-const contacts = [
-    {
-        firstName: "";
-        lastName: "";
-        phoneNumber: "";
-        dateNumber: "";
-    },
+// const contacts = [
+//     {
+//         firstName: "";
+//         lastName: "";
+//         phoneNumber: "";
+//         dateNumber: "";
+//     },
 
-    {
-        firstName: "";
-        lastName: "";
-        phoneNumber: "";
-        dateNumber: "";
-    },
+//     {
+//         firstName: "";
+//         lastName: "";
+//         phoneNumber: "";
+//         dateNumber: "";
+//     },
 
-    {
-        firstName: "";
-        lastName: "";
-        phoneNumber: "";
-        dateNumber: "";
-    },
+//     {
+//         firstName: "";
+//         lastName: "";
+//         phoneNumber: "";
+//         dateNumber: "";
+//     },
     
-    {
-        firstName: "";
-        lastName: "";
-        phoneNumber: "";
-        dateNumber: "";
-    },
-]
+//     {
+//         firstName: "";
+//         lastName: "";
+//         phoneNumber: "";
+//         dateNumber: "";
+//     },
+// ]
 
-const Contact = ({firstName, lastName, phoneNumber, date}) => {
-    return <li key={Contact}>
-    <h3>{firstName}</h3>
-    <div>
-      <strong>{lastName}</strong>
-    </div>
-    <div>{phoneNumber}</div>
-    <div>{date} </div>
-  </li>
-}
+// const Contact = ({firstName, lastName, phoneNumber, date}) => {
+//     return <li key={Contact}>
+//     <h3>{firstName}</h3>
+//     <div>
+//       <strong>{lastName}</strong>
+//     </div>
+//     <div>{phoneNumber}</div>
+//     <div>{date} </div>
+//   </li>
+// }
 
-const ContactList = () => {
-    return (
-    <ul>
-        {contacts.map((contact) => {
-        return <Contact {...contact} />
-        })}
-    </ul>
+// const ContactList = () => {
+//     return (
+//     <ul>
+//         {contacts.map((contact) => {
+//         return <Contact {...contact} />
+//         })}
+//     </ul>
 
-};
+// };
 
-export default ContactList;
+// export default ContactList;
